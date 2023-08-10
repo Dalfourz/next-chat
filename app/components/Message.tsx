a8a38ac922a4fad083b55f93addbf2b26c328a00
Dalfour <yjulius.mail@gmail.com>
2023-08-10 19:29:26 +1000

    optimise Message component

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -15,17 +15,20 @@ interface MessageProps {
   message: MessageData;
 }
 
-export default function Message({message}: MessageProps) {
-    
-  const [user] = useAuthState(auth)
+export default function Message({ message }: MessageProps) {
+  const [user] = useAuthState(auth);
+  const currentUser = user || null;
+  
+  const isCurrentUser = currentUser && message.uid === currentUser.uid;
+  const chatBubbleClassName = `flex px-2 py-1 ${isCurrentUser ? "chat__bubble--right" : ""}`;
 
   return (
-    <div className={`flex px-2 py-1 "" ${message.uid === user.uid ? "chat__bubble--right" : ""}`}>
-        <img src={message.avatar} alt="user avatar" className=""/>
-        <div className=" px-2">
-            <p className="sm:text-2xl font-semibold">{message.name}</p>
-            <p className="sm:text-2xl">{message.text}</p>
-        </div>
+    <div className={chatBubbleClassName}>
+      <img src={message.avatar} alt={`${message.name}'s avatar`} className="avatar" />
+      <div className="px-2">
+        <p className="sm:text-2xl font-semibold">{message.name}</p>
+        <p className="sm:text-2xl">{message.text}</p>
+      </div>
     </div>
-  )
+  );
 }
